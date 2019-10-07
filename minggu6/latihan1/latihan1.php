5eca3021d0a220b7a6002c1c1699853d8f3d4580
novayastika23 <novayastika99@gmail.com>
2019-10-07 11:48:57 +0700

    Latihan1

---
+++ minggu6/latihan1/latihan1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title> Contoh Skrip PHP </title>
+</head>
+<body>
+    <?PHP
+    $nama = "Kim Taehyung";
+    echo "$nama berkata \"save me save me\" ";
+    ?>
+</body>
+</html>
